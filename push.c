1c80c2e2b7c2e934ad4281be564e813c35fc033e
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-09-29 15:40:21 +0100

    stacking

--- push.c
+++ push.c
@@ -2,47 +2,47 @@
 #include <stdio.h>
 #include "monty.h"
 
-
 /**
- * push - push to stack function
- * @stack: double pointer to the stack
- * @line_number: line num in the bytecode file
- * @arg: what to push
+ * push - Pushes an element to the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number of the opcode in the bytecode file
+ * @arg: Argument to push (integer)
+ *
+ * Return: void
  */
 void push(stack_t **stack, unsigned int line_number, char *arg)
 {
-    stack_t *new_node;
-    int value;
+	stack_t *new_node;
+	int value;
 
-    // Check if the argument is NULL or not a valid integer
-    if (arg == NULL || !_is_number(arg))
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	/* Check if the argument is NULL or not a valid integer */
+	if (arg == NULL || !_is_number(arg))
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    value = atoi(arg);  // Convert the argument to an integer
+	value = atoi(arg);  /* Convert the argument to an integer */
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    // Initialize the new node
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	/* Initialize the new node */
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    // If the stack is not empty, update the previous top node
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
+	/* If the stack is not empty, update the previous top node */
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
 
-    // The new node becomes the top of the stack
-    *stack = new_node;
+	/* The new node becomes the top of the stack */
+	*stack = new_node;
 }
-
 
 /**
  * _is_number - Checks if a string is a valid number
@@ -52,17 +52,17 @@ void push(stack_t **stack, unsigned int line_number, char *arg)
  */
 int _is_number(char *str)
 {
-    if (str == NULL || *str == '\0')
-        return 0;
+	if (str == NULL || *str == '\0')
+		return (0);
 
-    if (*str == '-' || *str == '+')
-        str++;
+	if (*str == '-' || *str == '+')
+		str++;
 
-    while (*str)
-    {
-        if (*str < '0' || *str > '9')
-            return 0;
-        str++;
-    }
-    return 1;
+	while (*str)
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
 }
