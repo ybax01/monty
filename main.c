92de9036ed503c68731b045f0c576dce3b1152d2
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-09-29 16:26:15 +0100

    stacking

--- main.c
+++ main.c
@@ -4,39 +4,6 @@
 #include "monty.h"
 
 /**
- * execute_opcode - Executes the corresponding function for the given opcode
- * @opcode: The opcode to execute
- * @stack: Double pointer to the stack
- * @line_number: Line number of the opcode in the bytecode file
- * @arg: Argument to pass to the function (if any)
- *
- * Return: void
- */
-void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number, char *arg)
-{
-	instruction_t instructions[] = {
-		{"push", push},
-		{"pall", pall},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (instructions[i].opcode != NULL)
-	{
-		if (strcmp(opcode, instructions[i].opcode) == 0)
-		{
-			instructions[i].f(stack, line_number, arg);
-			return;
-		}
-		i++;
-	}
-
-	/* If the opcode is not found */
-	fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
-	exit(EXIT_FAILURE);
-}
-
-/**
  * main - Monty bytecode interpreter
  * @argc: Number of arguments passed to the program
  * @argv: Array of argument strings
--- monty.h
+++ monty.h
@@ -1,11 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
  * @next: points to the next element of the stack (or queue)
- *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
  */
@@ -19,7 +21,6 @@ typedef struct stack_s
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
- *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
